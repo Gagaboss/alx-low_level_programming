650f1375a3d0dfc0d80b04b0888856705a87b690
Gagaboss <egyake41444@gmail.com>
2023-06-16 02:34:30 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,13 @@
-#include<stdio.h>
-#include<stdio.h>
+#include <stdio.h>
+#include <unistd.h>
 /**
+ * main -Entry point
  *
- * main - A c program thaqt prints a line to the standard error
- * Return: 1 (success)
+ * Return: Always 0 (success)
  */
 int main(void)
 {
-	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19"\n", stdout);
-	return (1);
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+sizeof("and that piece of art is useful\" - Dora Korpar, 2015-10-19"));
+return (1);
 }
