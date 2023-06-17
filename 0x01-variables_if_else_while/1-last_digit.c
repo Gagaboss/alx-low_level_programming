7bcb3297eab48baa6636d2c1f1eb5e3b27fa077e
Gagaboss <egyake41444@gmail.com>
2023-06-17 07:11:35 -0700

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -25,7 +25,7 @@ int main(void)
 	}
 	else if (lastd > 6 && lastd != 0)
 	{
-	printf("last digit of %d is %d and is greater than 6 and not 0\n", n, lastd);
+	printf("last digit of %d is %d and is greater than 6 and not 8\n", n, lastd);
 	}
 	return (0);
 }
