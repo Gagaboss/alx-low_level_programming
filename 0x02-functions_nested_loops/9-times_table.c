21d6eb61fb3129fc491350496fd326d89feb8e70
Gagaboss <egyake41444@gmail.com>
2023-06-21 22:44:55 -0700

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -5,7 +5,7 @@
  * Return: empty output
  */
 
-void times_tables(void)
+void times_table(void)
 
 {
 
@@ -15,7 +15,7 @@ void times_tables(void)
 	{
 	for (y = 0; y <= 9; y++)
 	{
-	z = x / y;
+	z = x * y;
 	if (z > 9)
 	{ 
 	u = z % 10;
