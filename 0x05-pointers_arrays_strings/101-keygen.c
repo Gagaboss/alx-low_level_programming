692829fa5b33d2c98adc012ceafe1b091f0047ab
Gagaboss <egyake41444@gmail.com>
2023-06-27 22:54:09 -0700

    101-keygen.c

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -5,19 +5,19 @@
 /**
  * main - Generates random valid passwords for the
  * program 101-crackme.
+ *
  * Return: Always 0.
  */
 
 int main(void)
 
 {
-	char pass[84];
+	char password[84];
 	int index = 0, sum = 0, diff_half1, diff_half2;
 
 	srand(time(0));
 
 	while (sum < 2772)
-
 	{
 		password[index] = 33 + rand() % 94;
 		sum += password[index++];
@@ -29,7 +29,6 @@ int main(void)
 	{
 		diff_half1 = (sum - 2772) / 2;
 		diff_half2 = (sum - 2772) / 2;
-
 		if ((sum - 2772) % 2 != 0)
 		diff_half1++;
 
@@ -39,18 +38,14 @@ int main(void)
 			if (password[index] >= (33 + diff_half1))
 			{
 				password[index] -= diff_half1;
-
 				break;
-
 			}
 		}
 		for (index = 0; password[index]; index++)
 		{
 			if (password[index] >= (33 + diff_half2))
-
 			{
 				password[index] -= diff_half2;
-
 				break;
 			}
 		}
