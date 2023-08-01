57ece1fec3fb3e078491ea37124a85ec0360847b
Gagaboss <egyake41444@gmail.com>
2023-08-01 08:36:24 -0700

    101-print_listint_safe.c

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -5,50 +5,51 @@ size_t looped_listint_len(const listint_t *head);
 size_t print_listint_safe(const listint_t *head);
 
 /**
- * looped_listint_len - Counts the number of unique node
+ * looped_listint_len - Counts the number of unique nodes
  * in a looped listint_t linked list.
  * @head: A pointer to the head of the listint_t to check.
  *
- * Return: if the list is not looped - 0.
- * otherwise - the number of unique nodes in the list.
+ * Return: If the list is not looped - 0.
+ * Otherwise - the number of unique nodes in the list.
  */
 size_t looped_listint_len(const listint_t *head)
 {
 	const listint_t *tortoise, *hare;
 	size_t nodes = 1;
 
-	 if (head == NULL || head->next == NULL)
-		 return (0);
+	if (head == NULL || head->next == NULL)
+		return (0);
 
-	 tortoise = head->next;
-	 hare = (head->next)->next;
+	tortoise = head->next;
+	hare = (head->next)->next;
 
-	 while(hare)
-	 {
-		 if (tortoise == hare)
-		 {
-			 tortoise = head;
-			 while (tortoise != hare)
-			 {
-				 nodes++;
-				 tortoise = tortoise->next;
-				 hare = hare->next;
-			 }
+	while (hare)
+	{
+		if (tortoise == hare)
+		{
+			tortoise = head;
+			while (tortoise != hare)
+			{
+				nodes++;
+				tortoise = tortoise->next;
+				hare = hare->next;
+			}
 
-			 tortoise = tortoise->next;
-			 while (tortoise != hare)
-			 {
-				 nodes++
-					 tortoise = tortoise->next;
-			 }
+			tortoise = tortoise->next;
+			while (tortoise != hare)
+			{
+				nodes++;
+				tortoise = tortoise->next;
+			}
 
-			 return (nodes);
-		 }
+			return (nodes);
+		}
 
-		 tortoise = tortoise->next;
-		 hare = (hare->next)->next;
-	 }
-	 return (0);
+		tortoise = tortoise->next;
+		hare = (hare->next)->next;
+	}
+
+	return (0);
 }
 
 /**
@@ -63,7 +64,7 @@ size_t print_listint_safe(const listint_t *head)
 
 	nodes = looped_listint_len(head);
 
-	if(nodes == 0)
+	if (nodes == 0)
 	{
 		for (; head != NULL; nodes++)
 		{
